6d8e81054c9a2ead5d54e6e2b23a12babeb71f03
Smango <32824303+TheSmango@users.noreply.github.com>
2023-06-02 13:34:08 -0400

    Update RockyServerSetup.sh
    Added the ability to update almost any linux distro

--- RockyServerSetup.sh
+++ RockyServerSetup.sh
@@ -1,44 +1,112 @@
 #!/bin/bash
 
-echo "First we are going to install cockpit"
-sleep 5
-sudo dnf install cockpit -y
-sleep 3
-echo "Now let's enable cockpit to run on start-up or after reboot"
-sleep 5
-sudo systemctl enable --now cockpit.socket
-sleep 3
-echo "Let's enable the firewall to allow access to cockpit"
-sleep 5
-sudo firewall-cmd --add-service=cockpit
-sudo firewall-cmd --add-service=cockpit --permanent
-sleep 3
-echo "I like being able to install extra things on my installs so let's enable epel-release"
-sleep 5
-sudo dnf install epel-release -y
-sleep 5
-echo "Neofetch is awesome and a good way to know what kernal and version of linux you are running, let's install it next"
-sleep 3
-sudo dnf install neofetch -y
-sleep 3
-echo "Network Manager is a powerful tool that will help you setup your hostname and ip address extremely easy."
-sleep 5
-sudo dnf install NetworkManager-tui -y
-sleep 3
-echo "Let's run network manager now and setup our hostname."
-sleep 5
-sudo nmtui
-sleep 5
-echo "Now, lets make sure OpenSSH is working so we can connect to the server remotely"
-sleep 5
-sudo dnf install openssh-server -y
-sleep 1
-sudo systemctl start sshd
-sleep 1
-sudo systemctl enable sshd
-sleep 1
-sudo firewall-cmd --zone=public --permanent --add-service=ssh
-sleep 3
-echo "This basic setup script for Rocky Linux is now complete. You should now reboot and make sure everything is working as intended"
+echo "First, we are going to install Cockpit"
 sleep 5
 
+# Check package manager and install Cockpit
+if command -v dnf &>/dev/null; then
+  sudo dnf install cockpit -y
+elif command -v apt-get &>/dev/null; then
+  sudo apt-get install cockpit -y
+else
+  echo "Unsupported package manager. Please install Cockpit manually."
+  exit 1
+fi
+
+sleep 3
+echo "Now let's enable Cockpit to run on start-up or after reboot"
+sleep 5
+
+# Enable Cockpit service
+if command -v systemctl &>/dev/null; then
+  sudo systemctl enable --now cockpit.socket
+else
+  echo "Unsupported init system. Please enable Cockpit service manually."
+fi
+
+sleep 3
+echo "Let's enable the firewall to allow access to Cockpit"
+sleep 5
+
+# Enable firewall rules for Cockpit
+if command -v firewall-cmd &>/dev/null; then
+  sudo firewall-cmd --add-service=cockpit
+  sudo firewall-cmd --add-service=cockpit --permanent
+else
+  echo "Unsupported firewall. Please configure firewall rules for Cockpit manually."
+fi
+
+sleep 3
+echo "Next, let's enable the EPEL repository"
+sleep 5
+
+# Install epel-release
+if command -v dnf &>/dev/null; then
+  sudo dnf install epel-release -y
+elif command -v apt-get &>/dev/null; then
+  sudo apt-get install epel-release -y
+else
+  echo "Unsupported package manager. Please install epel-release manually."
+fi
+
+sleep 5
+echo "Neofetch is awesome and a good way to know what kernel and version of Linux you are running, let's install it next"
+sleep 3
+
+# Install neofetch
+if command -v dnf &>/dev/null; then
+  sudo dnf install neofetch -y
+elif command -v apt-get &>/dev/null; then
+  sudo apt-get install neofetch -y
+else
+  echo "Unsupported package manager. Please install neofetch manually."
+fi
+
+sleep 3
+echo "Network Manager is a powerful tool that will help you set up your hostname and IP address extremely easily."
+sleep 5
+
+# Install NetworkManager-tui
+if command -v dnf &>/dev/null; then
+  sudo dnf install NetworkManager-tui -y
+elif command -v apt-get &>/dev/null; then
+  sudo apt-get install network-manager -y
+else
+  echo "Unsupported package manager. Please install NetworkManager-tui manually."
+fi
+
+sleep 3
+echo "Let's run NetworkManager now and set up our hostname."
+sleep 5
+
+# Run nmtui to set up hostname
+if command -v nmtui &>/dev/null; then
+  sudo nmtui
+else
+  echo "nmtui is not available. Please configure NetworkManager and hostname manually."
+fi
+
+sleep 5
+echo "Now, let's make sure OpenSSH is working so we can connect to the server remotely"
+sleep 5
+
+# Install and enable OpenSSH server
+if command -v dnf &>/dev/null; then
+  sudo dnf install openssh-server -y
+elif command -v apt-get &>/dev/null; then
+  sudo apt-get install openssh-server -y
+else
+  echo "Unsupported package manager. Please install openssh-server manually."
+fi
+
+# Start and enable sshd service
+if command -v systemctl &>/dev/null; then
+  sudo systemctl start sshd
+  sudo systemctl enable sshd
+else
+  echo "Unsupported init system. Please start and enable sshd service manually."
+fi
+
+sleep 3
+echo "This basic setup script is now complete. You should now reboot and make sure everything is working as intended"
+sleep 5
